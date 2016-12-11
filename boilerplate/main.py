9832fa872a18516b319550cdc9e88c559b84af2e
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-12-11 14:52:53 +0100

    wrap bot to class

--- boilerplate/main.py
+++ boilerplate/main.py
@@ -11,93 +11,90 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('boilerplate-bot')
 logger.setLevel(logging.DEBUG)
 
-story = None
 
+class Bot:
+    def __init__(self):
+        self.story = botstory.Story()
+        stories.setup(self.story)
 
-def init(auto_start, fake_http_session):
-    global story
-    story = botstory.Story()
-    stories.setup(story)
+    def init(self, auto_start, fake_http_session):
+        self.story.use(fb.FBInterface(
+            # will show on initial screen
+            greeting_text='Hello dear {{user_first_name}}! '
+                          'I'' m demo bot of BotStory framework.',
+            # you should get on admin panel for the Messenger Product in Token Generation section
+            page_access_token=os.environ.get('FB_ACCESS_TOKEN', 'TEST_TOKEN'),
+            # menu of the bot that user has access all the time
+            persistent_menu=[{
+                'type': 'postback',
+                'title': 'Monkey Business',
+                'payload': 'MONKEY_BUSINESS'
+            }, {
+                'type': 'web_url',
+                'title': 'Source Code',
+                'url': 'https://github.com/botstory/bot-story/'
+            }],
+            # should be the same as in admin panel for the Webhook Product
+            webhook_url='/webhook{}'.format(os.environ.get('FB_WEBHOOK_URL_SECRET_PART', '')),
+            webhook_token=os.environ.get('FB_WEBHOOK_TOKEN', None),
+        ))
 
-    story.use(fb.FBInterface(
-        # will show on initial screen
-        greeting_text='Hello dear {{user_first_name}}! '
-                      'I'' m demo bot of BotStory framework.',
-        # you should get on admin panel for the Messenger Product in Token Generation section
-        page_access_token=os.environ.get('FB_ACCESS_TOKEN', 'TEST_TOKEN'),
-        # menu of the bot that user has access all the time
-        persistent_menu=[{
-            'type': 'postback',
-            'title': 'Monkey Business',
-            'payload': 'MONKEY_BUSINESS'
-        }, {
-            'type': 'web_url',
-            'title': 'Source Code',
-            'url': 'https://github.com/botstory/bot-story/'
-        }],
-        # should be the same as in admin panel for the Webhook Product
-        webhook_url='/webhook{}'.format(os.environ.get('FB_WEBHOOK_URL_SECRET_PART', '')),
-        webhook_token=os.environ.get('FB_WEBHOOK_TOKEN', None),
-    ))
+        # Interface for HTTP
+        http = self.story.use(aiohttp.AioHttpInterface(
+            port=int(os.environ.get('API_PORT', 8080)),
+            auto_start=auto_start,
+        ))
 
-    # Interface for HTTP
-    http = story.use(aiohttp.AioHttpInterface(
-        port=int(os.environ.get('API_PORT', 8080)),
-        auto_start=auto_start,
-    ))
+        # User and Session storage
+        self.story.use(mongodb.MongodbInterface(
+            uri=os.environ.get('MONGODB_URI', 'mongo'),
+            db_name=os.environ.get('MONGODB_DB_NAME', 'echobot'),
+        ))
 
-    # User and Session storage
-    story.use(mongodb.MongodbInterface(
-        uri=os.environ.get('MONGODB_URI', 'mongo'),
-        db_name=os.environ.get('MONGODB_DB_NAME', 'echobot'),
-    ))
+        self.story.use(tracker.GAStatistics(
+            tracking_id=os.environ.get('GA_ID'),
+        ))
 
-    story.use(tracker.GAStatistics(
-        tracking_id=os.environ.get('GA_ID'),
-    ))
+        # for test purpose
+        http.session = fake_http_session
 
-    # for test purpose
-    http.session = fake_http_session
+        # connect bot specific stories
 
-    # connect bot specific stories
+        logger.debug('self.story.stories_library.message_handling_stories')
+        logger.debug(self.story.stories_library.message_handling_stories)
 
-    logger.debug('story.stories_library.message_handling_stories')
-    logger.debug(story.stories_library.message_handling_stories)
+        return http
 
-    return http
+    async def setup(self, fake_http_session=None):
+        logger.info('setup')
+        self.init(auto_start=False, fake_http_session=fake_http_session)
+        await self.story.setup()
 
+    async def start(self, auto_start=True, fake_http_session=None):
+        http = self.init(auto_start, fake_http_session)
+        # start bot
+        await self.story.start()
 
-async def setup(fake_http_session=None):
-    logger.info('setup')
-    init(auto_start=False, fake_http_session=fake_http_session)
-    await story.setup()
+        logger.info('started')
 
+        return http.app
 
-async def start(auto_start=True, fake_http_session=None):
-    http = init(auto_start, fake_http_session)
-    # start bot
-    await story.start()
-
-    logger.info('started')
-
-    return http.app
-
-
-async def stop():
-    logger.info('stop')
-    await story.stop()
-    story.clear()
+    async def stop(self):
+        logger.info('stop')
+        await self.story.stop()
+        self.story.clear()
 
 
 def main(forever=True):
+    bot = Bot()
     logging.basicConfig(level=logging.DEBUG)
 
     loop = asyncio.get_event_loop()
-    app = loop.run_until_complete(start(auto_start=forever))
+    app = loop.run_until_complete(bot.start(auto_start=forever))
 
     # and run forever
     if forever:
-        story.forever(loop)
+        bot.story.forever(loop)
 
     # or you can use gunicorn for an app of http interface
     return app
