84b197b6778fd37bd23bf445653ab8235d215b9f
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-12-15 01:07:39 +0100

    add webpack build

---
+++ static/config/webpack.prod.js
@@ -0,0 +1,47 @@
+require('babel-polyfill');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// Webpack config for creating the production bundle.
+var path = require('path');
+var webpack = require('webpack');
+// var CleanPlugin = require('clean-webpack-plugin');
+
+var projectRootPath = path.resolve(__dirname, '../');
+var assetsPath = path.resolve(projectRootPath, './dist');
+
+// https://github.com/halt-hammerzeit/webpack-isomorphic-tools
+// var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
+// var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
+
+module.exports = {
+  devtool: 'source-map',
+  context: path.resolve(__dirname, '..'),
+  entry: {
+    'main': [
+      // 'bootstrap-sass!./src/theme/bootstrap.config.prod.js',
+      // 'font-awesome-webpack!./src/theme/font-awesome.config.prod.js',
+      './src/client.js'
+    ]
+  },
+  output: {
+    path: assetsPath,
+    filename: '[name]-[chunkhash].js',
+    chunkFilename: '[name]-[chunkhash].js',
+    publicPath: '/dist/'
+  },
+  module: {
+    loaders: [
+    ]
+  },
+  resolve: {
+    modules: [
+      'node_modules'
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
